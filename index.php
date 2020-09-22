925690df465cef729800020a63c362c2ece8671a
Khiêm <khiemphd@gmail.com>
2020-09-22 21:03:03 +0700

    l

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bài tập buổi 1</title>
+    <title>BÀI TẬP VỀ NHÀ</title>
 </head>
 <body>
     <a href="./BUOI1/BT-01.html">Bài tập 1</a> <br>
