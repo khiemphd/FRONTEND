dccce5e8863d5bb7cd72057f467e19ff4ac98dcb
Khiêm <khiemphd@gmail.com>
2020-09-22 20:16:29 +0700

    up

---
+++ BUOI1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập buổi 1</title>
+</head>
+<body>
+    <a href="./BTBuoi1/BT-01.html">Bài tập 1</a> <br>
+    <a href="./BTBuoi1/BT-02.html">Bài tập 2</a> <br>
+    <a href="./BTBuoi1/BT-03.html">Bài tập 3</a> <br>
+    <a href="./BTBuoi1/BT-04.html">Bài tập 4</a> <br>
+    <a href="./BTBuoi1/BT-05.html">Bài tập 5</a> <br>
+    <a href="./BTBuoi1/BT-06.html">Bài tập 6</a> <br>
+    <a href="./BTBuoi1/BT-07.html">Bài tập 7</a> <br>
+    <a href="./BTBuoi1/BT-08.html">Bài tập 8</a> <br>
+    <a href="./BTBuoi1/BT-09.html">Bài tập 9</a>
+</body>
+</html>
