67d26b9233fafa693161eb876bfec2aa87d1db9a
Khiêm <khiemphd@gmail.com>
2020-09-22 20:29:12 +0700

    m

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <title>Bài tập buổi 1</title>
 </head>
 <body>
-    <a href="./BTBuoi1/BT-01.html">Bài tập 1</a> <br>
-    <a href="./BTBuoi1/BT-02.html">Bài tập 2</a> <br>
-    <a href="./BTBuoi1/BT-03.html">Bài tập 3</a> <br>
-    <a href="./BTBuoi1/BT-04.html">Bài tập 4</a> <br>
-    <a href="./BTBuoi1/BT-05.html">Bài tập 5</a> <br>
-    <a href="./BTBuoi1/BT-06.html">Bài tập 6</a> <br>
-    <a href="./BTBuoi1/BT-07.html">Bài tập 7</a> <br>
-    <a href="./BTBuoi1/BT-08.html">Bài tập 8</a> <br>
-    <a href="./BTBuoi1/BT-09.html">Bài tập 9</a>
+    <a href="./BUOI1/BT-01.html">Bài tập 1</a> <br>
+    <a href="./BUOI1/BT-02.html">Bài tập 2</a> <br>
+    <a href="./BUOI1/BT-03.html">Bài tập 3</a> <br>
+    <a href="./BUOI1/BT-04.html">Bài tập 4</a> <br>
+    <a href="./BUOI1/BT-05.html">Bài tập 5</a> <br>
+    <a href="./BUOI1/BT-06.html">Bài tập 6</a> <br>
+    <a href="./BUOI1/BT-07.html">Bài tập 7</a> <br>
+    <a href="./BUOI1/BT-08.html">Bài tập 8</a> <br>
+    <a href="./BUOI1/BT-09.html">Bài tập 9</a>
 </body>
 </html>
